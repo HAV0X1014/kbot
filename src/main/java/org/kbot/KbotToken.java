f3e13b1e86cf88d16d84bcdba6ae7681c4ad8bf8
HAV0X underscore <73144620+HAV0X1014@users.noreply.github.com>
2023-03-28 19:25:24 -0400

    only java in the java branch

---
+++ src/main/java/org/kbot/KbotToken.java
@@ -0,0 +1,17 @@
+package org.kbot;
+
+import java.io.*;
+
+public class KbotToken {
+    public static String read(String s) {
+        try {
+            FileReader file = new FileReader(s);
+            BufferedReader buffer = new BufferedReader(file);
+
+            return buffer.readLine();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
