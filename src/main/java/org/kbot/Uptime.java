6f8ff4f9058282e1844fd3dbb0aa5c2fea6cba6d
HAV0X underscore <73144620+HAV0X1014@users.noreply.github.com>
2023-03-28 19:10:52 -0400

    Jarva
    jarva

---
+++ src/main/java/org/kbot/Uptime.java
@@ -0,0 +1,15 @@
+package org.kbot;
+
+import java.time.Duration;
+
+public class Uptime {
+    public static String uptime(int startTime){
+        int systemTime = (int) (System.currentTimeMillis() / 1000);
+        long upTimeSeconds = systemTime - startTime;
+        Duration duration = Duration.ofSeconds(upTimeSeconds);
+        long hours = duration.toHours();
+        long minutes = duration.minusHours(hours).toMinutes();
+        String upTime = String.format("``%04d`` hours, ``%02d`` minutes", hours, minutes);
+        return upTime;
+    }
+}
